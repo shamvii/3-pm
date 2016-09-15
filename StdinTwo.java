a2d9ca220688e4b0e999cac49f163acc70e03aed
shamvii <shambhavishukla24@gmail.com>
2016-09-15 02:15:54 -0400

    next line and Parse usage

---
+++ StdinTwo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        
+        
+       //  int i = Integer.parseInt(scan.nextLine());
+        // double d = Double.parseDouble(scan.nextLine());
+         //String s = scan.nextLine();
+        //scan.nextLine();
+        
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
